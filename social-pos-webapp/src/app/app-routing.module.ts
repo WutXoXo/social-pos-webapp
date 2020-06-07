bf9d6e23192f34cc0c605c4e8b7fc6c7bacf3963
Worawut Ouncharoen <worawut.oun@gmail.com>
2020-06-08 00:13:25 +0700

    add page login

--- social-pos-webapp/src/app/app-routing.module.ts
+++ social-pos-webapp/src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/app',pathMatch:'full'},
+  {
+    path: 'app',
+    component: HomeComponent,
+    children: [ 
+      {
+        path: '',
+        component: DashboardComponent
+      },    
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]    
+  },
+  {path:'login',component:LoginComponent},
+  {path:'**',redirectTo:'/add'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- social-pos-webapp/src/app/app.module.ts
+++ social-pos-webapp/src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { MenuComponent } from './components/home/menu/menu.component';
+import { FooterComponent } from './components/home/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
